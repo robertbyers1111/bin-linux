e58c202573491fbfd4b87b5efbbb22e23b25e62c
robertbyers1111 <robertbyers1111@gmail.com>
2021-08-15 16:07:00 -0400

    Add babletrace warning

--- .search_logfile_for_errors.sh
+++ .search_logfile_for_errors.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+[ $# -ne 1 ] && { echo; echo "        USAGE: `basename $0` file"; echo; exit; }
+
 cat "$1" \
  | col -bx \
- | grep -Eiv "^  *-w error |help.*locale.*cannot find document:|locale is free of encoding errors|^/mtkoss/gcc/.*/bin/g++.*errors.*errors.*errors.*errors| errors\.o " \
+ | grep -Eiv "^  *-w error |help.*locale.*cannot find document:|locale is free of encoding errors|^/mtkoss/gcc/.*/bin/g++.*errors.*errors.*errors.*errors| errors\.o |configure: WARNING: babletrace support disabled" \
  | gvim -geometry=184x45+4+118 -s ~/bin/searchfor_ERROR.vim -
