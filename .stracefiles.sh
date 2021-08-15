37e34c497099521b5bcc96345ba9a5b752814246
robertbyers1111 <robertbyers1111@gmail.com>
2021-08-15 16:23:24 -0400

    .stracefiles.sh: Cleaned up several minor bugs. Renamed some output files

--- .stracefiles.sh
+++ .stracefiles.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # This script extracts all the names of files referenced in an strace output file
+#
+# WARNING: Not designed for spaces in filenames
+#
+# TODO: 'cannot open' messages should be part of the errors file, not the directories file
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # doit() identifies the file types of all discored files
@@ -22,34 +26,34 @@ doit()
 
 TMPDIR=.
 TMPFILEBASE=`mktemp -u --tmpdir=$TMPDIR -t $(basename $0)_${USER}_$(echo 10i16o$(date +%s%N)p|dc)XXXX`
-TMPFILE_A=${TMPFILEBASE}_all.tmp
-TMPFILE_E=${TMPFILEBASE}_errs.tmp
-TMPFILE_N0=${TMPFILEBASE}_noErrsA.tmp
-TMPFILE_N1=${TMPFILEBASE}_noErrs1.tmp
-TMPFILE_N2=${TMPFILEBASE}_noErrsF.tmp
-TMPFILE_N3=${TMPFILEBASE}_noErrsD.tmp
-TMPFILE_O=${TMPFILEBASE}_organized.tmp
+TMPFILE_A=${TMPFILEBASE}_all.log
+TMPFILE_E=${TMPFILEBASE}_errs.log
+TMPFILE_N0=${TMPFILEBASE}_noErrsA.log
+TMPFILE_N1=${TMPFILEBASE}_noErrs1.log
+TMPFILE_N2=${TMPFILEBASE}_noErrsF.log
+TMPFILE_N3=${TMPFILEBASE}_noErrsD.log
+TMPFILE_NE=${TMPFILEBASE}_noErrors.log
 
 #First grep of the trace file..
 grep \" "$1" | grep -Ev '/__pycache__/|read\(|write\(|read resumed|getrandom' > $TMPFILE_A
 
-echo " [`date +%H:%M:%S.%N | cut -c-12`]                 All files: $TMPFILE_A (`wc -l $TMPFILE_A | cut -d\  -f1`)"
+#DEBUG echo " [`date +%H:%M:%S.%N | cut -c-12`]                 All files: $TMPFILE_A (`wc -l $TMPFILE_A | cut -d\  -f1`)"
 
 #Find errors
-    grep -E ')\s*=\s*\S+\s*E' $TMPFILE_A \
-     | cut -d\" -f2 \
+    grep -E '\)\s*=\s*\S+\s*E' $TMPFILE_A \
+     | cut -d\" -f2- \
      | sort -Vu \
      > $TMPFILE_E
 
     echo " [`date +%H:%M:%S.%N | cut -c-12`]                    Errors: $TMPFILE_E (`wc -l $TMPFILE_E | cut -d\  -f1`)"
 
 #Find non-errors
-    grep -Ev ')\s*=\s*\S+\s*E' $TMPFILE_A \
+    grep -Ev '\)\s*=\s*\S+\s*E' $TMPFILE_A \
      | cut -d\" -f2 \
      | sort -Vu \
      > $TMPFILE_N0
 
-    echo " [`date +%H:%M:%S.%N | cut -c-12`]                Non-errors: $TMPFILE_N0 (`wc -l $TMPFILE_N0 | cut -d\  -f1`)"
+    #DEBUG echo " [`date +%H:%M:%S.%N | cut -c-12`]                Non-errors: $TMPFILE_N0 (`wc -l $TMPFILE_N0 | cut -d\  -f1`)"
 
 #Post-processing to organize the results by file type
 
@@ -63,9 +67,10 @@ echo " [`date +%H:%M:%S.%N | cut -c-12`]                 All files: $TMPFILE_A (
         echo
         echo [Directories and cannot-opens] =========================================
         cat $TMPFILE_N3
-    } > $TMPFILE_O
+    } > $TMPFILE_NE
 
-    echo " [`date +%H:%M:%S.%N | cut -c-12`]     Files from non-errors: $TMPFILE_N2 (`wc -l $TMPFILE_N2 | cut -d\  -f1`)"
-    echo " [`date +%H:%M:%S.%N | cut -c-12`] Non-Files from non-errors: $TMPFILE_N3 (`wc -l $TMPFILE_N3 | cut -d\  -f1`)"
-    echo " [`date +%H:%M:%S.%N | cut -c-12`]      Organized non-errors: $TMPFILE_O (`wc -l $TMPFILE_O | cut -d\  -f1`)"
-    gvim $TMPFILE_O
+    #DEBUG echo " [`date +%H:%M:%S.%N | cut -c-12`]     Files from non-errors: $TMPFILE_N2 (`wc -l $TMPFILE_N2 | cut -d\  -f1`)"
+    #DEBUG echo " [`date +%H:%M:%S.%N | cut -c-12`] Non-Files from non-errors: $TMPFILE_N3 (`wc -l $TMPFILE_N3 | cut -d\  -f1`)"
+    echo " [`date +%H:%M:%S.%N | cut -c-12`]                Non-Errors: $TMPFILE_NE (`wc -l $TMPFILE_NE | cut -d\  -f1`)"
+    gvim -geometry 144x40+128+128  $TMPFILE_NE
+
