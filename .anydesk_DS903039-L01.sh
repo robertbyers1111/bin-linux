d53efe1cba5d5da6e184e4ca7d1c8edfe6cd8f33
robertbyers1111 <robertbyers1111@gmail.com>
2021-08-31 15:27:08 -0400

    Fix to use correct TCP port

--- .anydesk_DS903039-L01.sh
+++ .anydesk_DS903039-L01.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Launch an anydesk session to my mediatek laptop using its IP and port number
 MEDIATEK_PC_IPADDR=10.0.0.28
-MEDIATEK_PC_TCPPORT=7070
+MEDIATEK_PC_TCPPORT=5977
 CMD="anydesk ${MEDIATEK_PC_IPADDR}:${MEDIATEK_PC_TCPPORT}"
 echo $CMD
 $CMD &
