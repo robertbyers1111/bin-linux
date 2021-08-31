053a19638ddc0a8514ede4da35eeb41b45c0f3a0
robertbyers1111 <robertbyers1111@gmail.com>
2021-08-31 15:32:23 -0400

    Rename in order to reduce tab completion conflicts

--- .gShowFilesInCommits.sh
+++ .gShowFilesInCommits.sh
@@ -2,8 +2,14 @@
 
 # Displays the files affected by the last several commits to the current branch
 
-COMMITS=8
+COMMITS=32
 COMMIT_IDS=`git log --oneline | head -$COMMITS | awk '{ printf "%s\n",$1 }'`
+
+cat<<EOT
+
+        Running "git show --pretty='' --name-only" for each commit
+
+EOT
 
 for COMMIT_ID in $COMMIT_IDS
 do
