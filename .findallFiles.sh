294d7f4a8142609d4b4f8c813e32c76fee487166
robertbyers1111 <robertbyers1111@gmail.com>
2021-09-01 12:19:54 -0400

    Tailored for Mediatek only. Not finished

--- .findallFiles.sh
+++ .findallFiles.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-sudo find /bin /etc /home /lib* /opt /root /sbin /srv /usr /var -xdev $*
+#
+# Search the entire local filesystem (with certain exclusions)
+#
+# This version is for Mediatek
+#
+# 2021-0826 NOT WORKING! (does nothing)
+
+sudo find /admin /bin /etc /home/rbyers /lib* /opt /root /sbin /srv /usr /var -xdev \
+    -path /home/rbyers/.cache -prune -false \
+ -o -path /home/rbyers/.eclipse -prune -false \
+ -o -path /home/rbyers/.m2 -prune -false \
+ -o -path /home/rbyers/.snapshot -prune -false \
+ $*
+
