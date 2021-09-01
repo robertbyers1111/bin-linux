63b292afafd6b2bcf346a645564c0dc2eab9d5ce
robertbyers1111 <robertbyers1111@gmail.com>
2021-09-01 12:17:03 -0400

    Add support for anydesk CLI options

--- .anydesk_DS903039-L01.sh
+++ .anydesk_DS903039-L01.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Launch an anydesk session to my mediatek laptop using its IP and port number
+#
+# Anydesk CLI supports the following..
+#
+#    --file-transfer
+#    --fullscreen
+#    --plain (i.e., no window title and toolbar)
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 MEDIATEK_PC_IPADDR=10.0.0.28
 MEDIATEK_PC_TCPPORT=5977
-CMD="anydesk ${MEDIATEK_PC_IPADDR}:${MEDIATEK_PC_TCPPORT}"
+ANYDESK_OPTIONS="$*"
+CMD="anydesk ${ANYDESK_OPTIONS} ${MEDIATEK_PC_IPADDR}:${MEDIATEK_PC_TCPPORT}"
 echo $CMD
 $CMD &
 
