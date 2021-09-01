84d49ef074b6795ed4ac2da681e3e7f97fe94e8a
robertbyers1111 <robertbyers1111@gmail.com>
2021-09-01 12:25:30 -0400

    Adding .gl.sh

---
+++ .gl.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# git log with my own type of oneliner
+#
+
+doit()
+{
+    git log --pretty=format:'%ai  %<(14,trunc)%an  %h  %<|(155,trunc)%s' -32 $1 $*
+}
+
+case $1 in
+    --graph|-g|g) GRAPH=--graph
+    shift
+    ;;
+    *) [ $# -gt 0 ] && {
+        echo
+        echo "USAGE: `basename $0` [--graph|-g|g]"
+        echo
+        echo "   FWIW, this script uses the following form of 'git log'.."
+        echo
+        echo "        git log --pretty=format:'%ai  %<(14,trunc)%an  %h  %<|(155,trunc)%s' -32k"
+        echo
+        echo "        ai - timestamp of the commit"
+        echo "        an - commit author's name"
+        echo "         h - hash of the commit"
+        echo "         s - string of the commit message"
+        echo
+        exit
+    }
+    ;;
+esac
+
+doit $GRAPH $*
+
