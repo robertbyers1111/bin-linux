996cc066d8bb6ed014505a4ce64ac521fcf4f5ee
robertbyers1111 <robertbyers1111@gmail.com>
2021-09-08 10:13:55 -0400

    Renamed .gl1.sh to .glog.sh. Also, prettied-up its output

---
+++ .glog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo
+echo % git branch --verbose
+echo
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+git branch --verbose
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+echo
+echo % git log --graph --oneline --all --decorate=full
+echo
+#cho "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+git log --graph --oneline --all --decorate=full
+#cho "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+echo
