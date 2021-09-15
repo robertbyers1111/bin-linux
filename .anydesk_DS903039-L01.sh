cfe2485cd9cc35a568e385fcb18dab411fdd2612
robertbyers1111 <robertbyers1111@gmail.com>
2021-09-15 18:18:40 -0400

    Updated Mediatek IP Addr

--- .anydesk_DS903039-L01.sh
+++ .anydesk_DS903039-L01.sh
@@ -10,7 +10,7 @@
 #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-MEDIATEK_PC_IPADDR=10.0.0.28
+MEDIATEK_PC_IPADDR=10.0.0.19
 MEDIATEK_PC_TCPPORT=5977
 ANYDESK_OPTIONS="$*"
 CMD="anydesk ${ANYDESK_OPTIONS} ${MEDIATEK_PC_IPADDR}:${MEDIATEK_PC_TCPPORT}"
